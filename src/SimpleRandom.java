fb9d423cde231a9a41803aae721af59d6084e90e
Abhishek Miglani <42495528+abhishekmiglani@users.noreply.github.com>
2019-10-09 14:07:25 +0530

    Add files via upload

---
+++ src/SimpleRandom.java
@@ -0,0 +1,29 @@
+public class SimpleRandom {
+/**
+ * Test code
+ * @throws InterruptedException 
+ */
+public static void main(String[] args) throws InterruptedException {
+    SimpleRandom rand = new SimpleRandom(100);
+    for (int i = 0; i < 10; i++) {
+        System.out.print(rand.nextInt()+ " ");
+        Thread.sleep(1000);
+    }
+
+}
+
+private int max;
+private int last;
+
+// constructor that takes the max int
+public SimpleRandom(int max){
+    this.max = max;
+    last = (int) (System.currentTimeMillis() % max);
+}
+
+// Note that the result can not be bigger then 32749
+public int nextInt(){
+    last = (last * 32719 + 3) % 32750;
+    return last % max;
+}
+}
